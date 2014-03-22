c4b7ae059889b7719e82786c376ba46c748d3f03
John Kolovos <yannis.kolovos@gmail.com>
2014-03-22 21:01:30 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedKiteHeroes::Application.config.secret_key_base = '3fcabf7527f26732f7b4e7600e9e7534fe9a158b7e1adec6a5dd43b89b087caf0416a511f5d93bf1d514954df13b076deb5f25cb0d055b3a03255f0c0cb6a838'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedKiteHeroes::Application.config.session_store :cookie_store, key: '_RedKiteHeroes_session'
